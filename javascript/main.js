4e7bfb124c226f37bcf7abe6b2cc410779b746ad
Masaharu TASHIRO <masatsr.kit@gmail.com>
2021-01-09 22:39:10 +0900

    Shadowing in JavaScript

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+function main() {
+  console.log("return value of one() is " + one());
+  let one = 1;
+  console.log("value of one is " + one);
+  let two = 1 + 1;
+}
+
+function one() {
+  return 1;
+}
+
+main();
